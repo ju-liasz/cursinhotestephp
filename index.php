60d518702068dca1669fadcb4e8cb7ca79846748
unknown <Digital@DH-A10-36.dhbrasil.digitalhouse.com>
2019-09-27 12:04:30 -0300

    codeDaLojinha

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+        $nomeSistema = "Cursinhos";
+        ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Lojinha</title>
+</head>
+<body>
+    <header class = "d-flex justify-content-between align-items-center p-3">
+        <h1 id = "logo">
+         <?php echo $nomeSistema; ?>
+        </h1>
+        <nav> 
+        <ul class = "nav">
+        <li class="nav-item"> Cursos </li>
+        <li class="nav-item"> Login </li>
+        <li class="nav-item"> Cadastrar </li>
+        </ul>
+        </nav>
+ </header>
+
+</body>
+</html>"
